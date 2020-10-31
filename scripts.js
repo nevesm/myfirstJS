935a4aa1cc6a7e41aa99e42c856393ebf8534de8
nevesm <44535389+nevesm@users.noreply.github.com>
2020-10-31 04:45:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,6 @@ countryName = document.getElementById('searchInput').value;
 var urlendpoint = 'https://restcountries.eu/rest/v2/name/';
 var endpointfields = '?fields=topLevelDomain;alpha3Code;callingCodes;altSpellings;subregion;population;latlng;languages;flag';
 fetch(urlendpoint + countryName + endpointfields)
- .then(response => response.text())
+ .then(response => response.json())
  .then(data => console.log(data))
 }
