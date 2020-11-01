4d9917e0a26a8199ee374ed31ac8b03392a02d1e
nevesm <44535389+nevesm@users.noreply.github.com>
2020-11-01 05:24:44 -0300

    ye

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function grabCountry() {
 countryName = document.getElementById('searchInput').value;
 var urlendpoint = 'https://restcountries.eu/rest/v2/name/';
 var endpointfields = '?fields=topLevelDomain;alpha3Code;callingCodes;altSpellings;subregion;population;latlng;languages;flag';
-fetch(urlendpoint + countryName + endpointfields)
- .then(response => response.json())
- .then(data => console.log(data))
-document.getElementById("population").innerHTML = data.population;
+fetch(urlendpoint + countryName + endpointfields, { 
+  method: 'GET'
+})
+.then(function(response) { return response.json(); })
+.then(function(json) {
+console.log(json)
+});
 }
-
